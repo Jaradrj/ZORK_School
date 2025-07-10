9f5899ff362bc0a44e1c3f45d997a87981064b91
Jaradrj <jara.derooij@edu.tbz.ch>
2025-07-10 10:27:38 +0200

    fix commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,35 @@
 import controller.GameController;
 import game.Commands;
+import game.RoomFactory;
+import game.Room;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) {
-		Commands command = null;
-		GameController controller = new GameController(null);
+		Map<String, Room> roomMap = new HashMap<>();
+		String[] roomNames = {
+				"main entrance hall",
+				"music room",
+				"printer room",
+				"secretary",
+				"cafeteria",
+				"garage",
+				"it room",
+				"sportshall",
+				"chemistry room",
+				"electricity room",
+				"teacher room"
+		};
+
+		for (String name : roomNames) {
+			roomMap.put(name, RoomFactory.createRoom(name));
+		}
+
+		Commands commands = new Commands(roomMap);
+		GameController controller = new GameController(commands);
+		RoomFactory.setController(controller);
 		controller.run();
 	}
 }
