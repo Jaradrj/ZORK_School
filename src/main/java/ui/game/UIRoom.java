df03d0db45baa35ecff4a2d097794d1d0a598994
zshyl444 <jevgenia.shylovich@edu.tbz.ch>
2025-07-11 08:58:11 +0200

    add interface

---
+++ src/main/java/ui/game/UIRoom.java
@@ -0,0 +1,13 @@
+package ui.game;
+
+import console.game.Exit;
+import console.game.Player;
+import java.util.Map;
+
+public interface UIRoom {
+    String getName();
+    String enter(Player player);
+    String performAction(Player player, String action);
+    Map<String, Exit> getAvailableExits(Player player);
+    String handleRoomChange(Player player, String roomName);
+}
---
+++ src/main/java/ui/rooms/UIMainEntranceRoom.java
@@ -0,0 +1,4 @@
+package ui.rooms;
+
+public class UIMainEntranceRoom {
+}
